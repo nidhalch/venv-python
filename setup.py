54bc08d8c033740efa8a780accc4e43143ff5ecb
Cyril Levis <cyril.levis@dailymotion.com>
2020-02-28 17:04:53 +0100

    Fix check_redis_sentinel pip module

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
- 
- 
+
+
 """setup.py: setuptools control."""
- 
- 
+
+
 import re
 from setuptools import setup
- 
- 
-version = '0.0.1'
- 
- 
+
+
+version = "0.0.1"
+
+
 with open("README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
- 
- 
+
+
 setup(
-    name = "redis_sentinel_check_oio",
-    packages = ["redis_sentinel_check_oio"],
-    entry_points = {
-        "console_scripts": ['redis_sentinel_check_oio = redis_sentinel_check_oio.check_redis_sentinel_check:main']
-        },
-    version = version,
-    description = " Check redis sentinel on oIo servers.",
-    long_description = long_descr,
-    author = "DM",
-    author_email = "nidhal.chabchoubi@dailymotion.com",
-    )
+    name="check_redis_sentinel",
+    packages=["check_redis_sentinel"],
+    entry_points={
+        "console_scripts": [
+            "check_redis_sentinel = check_redis_sentinel.check_redis_sentinel:main"
+        ]
+    },
+    version=version,
+    description=" Check redis sentinel on servers.",
+    long_description=long_descr,
+    author="DM",
+    author_email="nidhal.chabchoubi@dailymotion.com",
+    python_requires=">=2.7",
+    install_requires="redis>=3.4.1",
+)
